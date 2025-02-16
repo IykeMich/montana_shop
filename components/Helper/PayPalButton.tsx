df9a23d68136e2d2826ac30327bcd85ee81e5419
IykeMich <iykemich361@gmail.com>
2025-02-16 19:01:15 +0100

    lil modification to the toast

--- components/Helper/PayPalButton.tsx
+++ components/Helper/PayPalButton.tsx
@@ -1,4 +1,49 @@
-// @ts-nocheck
+// import React from "react";
+// import {
+//   FUNDING,
+//   PayPalButtons,
+//   PayPalScriptProvider,
+// } from "@paypal/react-paypal-js";
+
+// interface PaypalButtonProps {
+//   amount: string;
+//   onSuccess: (details: any) => void;
+// }
+
+// const PayPalButton = ({ amount, onSuccess }: PaypalButtonProps) => {
+//   return (
+//     <PayPalScriptProvider
+//       options={{
+//         clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+//         currency: "USD",
+//       }}
+//     >
+//       <PayPalButtons fundingSource={FUNDING.PAYPAL} 
+//       createOrder={(data,actions) => {
+//         return actions.order.create({
+//           purchase_units:[
+//             {
+//               amount: {
+//                 value:amount
+//               }
+//             }
+//           ]
+//         })
+//       }} 
+//       onApprove = {(data,actions) => {
+//         return actions.order.capture().then((details) => {
+//           onSuccess(details)
+//         })
+//       }}
+      
+//       />
+
+//     </PayPalScriptProvider>
+//   );
+// };
+
+// export default PayPalButton;
+
 import React from "react";
 import {
   FUNDING,
@@ -11,34 +56,34 @@ interface PaypalButtonProps {
   onSuccess: (details: any) => void;
 }
 
-const PayPalButton = ({ amount, onSuccess }: PaypalButtonProps) => {
+const PayPalButton: React.FC<PaypalButtonProps> = ({ amount, onSuccess }) => {
   return (
     <PayPalScriptProvider
       options={{
-        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || "",
         currency: "USD",
       }}
     >
-      <PayPalButtons fundingSource={FUNDING.PAYPAL} 
-      createOrder={(data,actions) => {
-        return actions.order.create({
-          purchase_units:[
-            {
-              amount: {
-                value:amount
-              }
-            }
-          ]
-        })
-      }} 
-      onApprove = {(data,actions) => {
-        return actions.order.capture().then((details) => {
-          onSuccess(details)
-        })
-      }}
-      
+      <PayPalButtons
+        fundingSource={FUNDING.PAYPAL}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: amount,
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={(data, actions) => {
+          if (!actions.order) return Promise.reject();
+          return actions.order.capture().then((details) => {
+            onSuccess(details);
+          });
+        }}
       />
-
     </PayPalScriptProvider>
   );
 };
